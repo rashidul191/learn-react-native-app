5ae5379bcb944e440aa3f391095cfcc3c2ea5e8e
Rashidul Islam <rashidul.191cse.gub@gmail.com>
2026-05-18 19:13:09 +0600

    working form

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,17 +1,90 @@
-import { Text, View, StyleSheet } from "react-native";
+import { useState } from "react";
+import { Pressable, StyleSheet, Text, TextInput, View } from "react-native";
 
-export default function Index() {
+import { useForm, Controller } from "react-hook-form";
+
+const Index = () => {
+  const [count, setCount] = useState(0);
+
+  const { control, handleSubmit } = useForm();
+
+  const onSubmit = (data: any) => {
+    console.log(data);
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Edit src/app/index.tsx to edit this screen.</Text>
+    <View style={{ alignItems: "center", gap: 10 }}>
+      <Text style={styles.title}>Welcome</Text>
+
+      <Text>Count: {count}</Text>
+
+      <Pressable onPress={() => setCount(count + 1)} style={styles.button}>
+        <Text style={{ color: "white", textAlign: "center" }}>Count</Text>
+      </Pressable>
+
+      <View style={{ gap: 10 }}>
+        <Text>Login Form</Text>
+
+        <View>
+          <Text>Email *</Text>
+          <Controller
+            control={control}
+            name="name"
+            rules={{ required: "Name is required" }}
+            render={({ field: { onChange, value } }) => (
+              <TextInput
+                placeholder="Enter your name"
+                value={value}
+                onChangeText={onChange}
+                style={styles.input}
+              />
+            )}
+          />
+        </View>
+        <View>
+          <Text>Number</Text>
+          <Controller
+            control={control}
+            name="number"
+            render={({ field: { onChange, value } }) => (
+              <TextInput
+                keyboardType="numeric"
+                placeholder="Enter your number"
+                value={value}
+                onChangeText={onChange}
+                style={styles.input}
+              />
+            )}
+          />
+        </View>
+
+        <Pressable onPress={handleSubmit(onSubmit)} style={styles.button}>
+          <Text style={{ color: "white", textAlign: "center" }}>Submit</Text>
+        </Pressable>
+      </View>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
+  title: {
+    fontSize: 15,
+  },
+
+  button: {
+    backgroundColor: "red",
+    width: 100,
+    padding: 10,
+    borderRadius: 10,
+  },
+
+  input: {
+    borderWidth: 1,
+    borderColor: "#ccc",
+    padding: 10,
+    borderRadius: 10,
+    width: 250,
   },
 });
+
+export default Index;
